526a672fec41a7f10c5da0625dbbc03ce9299de5
Ramos <carlos.alberto.ramos1@dxc.com>
2020-09-10 16:29:29 -0600

    Agregado evento para cerrar vista de letra, y agregados título en sección de descarte

--- TiempoRealEventListener.py
+++ TiempoRealEventListener.py
@@ -1,3 +1,8 @@
+"""
+PENDIENTES:
+    Si el grupo de previsualizaciones se queda vacío, cerrar el grupo.
+"""
+
 import sublime
 import sublime_plugin
 
@@ -9,6 +14,17 @@ class TiempoRealViewEventListener(sublime_plugin.ViewEventListener):
         Al abrir una letra, se abre su preview.
         """
         self.view.run_command('abrir_letra_markdown_preview')
+
+    def on_pre_close(self):
+        """
+        Si hay una vista asociada abierta, la cierra.
+        Esto se hace en pre-close, de lo contrario hay error al encontrar la vista.
+        """
+        if not self.view.settings().has("preview_view_id"):
+            return
+
+        vistaPreview = self.__recuperarVistaDePreview()
+        vistaPreview.close()
 
     def on_modified_async(self):
         """
@@ -28,6 +44,8 @@ class TiempoRealViewEventListener(sublime_plugin.ViewEventListener):
 
     def __recuperarVistaDePreview(self):
         preview_view_id = self.view.settings().get("preview_view_id")
+        print("View name is: " + self.view.name())
+        print("View filename is: " + self.view.file_name())
         for vista in self.view.window().views():
             if vista.id() == preview_view_id:
                 return vista
